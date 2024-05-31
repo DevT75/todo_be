54bd42727778f230429da4fc69fbd40ab40af77c
Devendra <dekuta75@gmail.com>
2024-06-01 01:08:22 +0530

    updated vercel.json-5

--- main.py
+++ main.py
@@ -1,10 +1,1 @@
-import uvicorn
-from os import getenv
-
-if __name__ == '__main__':
-    uvicorn.run(
-        'app.app:app',
-        host=getenv('HOST', '0.0.0.0'),
-        port=int(getenv('PORT', 8000)),
-        reload=True
-    )
+from app.app import app
